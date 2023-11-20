557a927da4cc13f0f9e74aa5de146687a86a23a7
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-11-20 16:22:35 +0100

    feat: fin de creation du header.
    Attention : manque le transfert de la classe "current" sur le link de la page en cours.

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -4,13 +4,13 @@ import logo from "../../assets/logo.svg";
 function Header() {
     return (
       <header>
-        
+        <div className="header__content">
             <img src={logo} alt="Logo Kasa" />
             <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/a-propos">A propos</Link>
+                <Link to="/" className="current">Accueil</Link>
+                <Link to="/a-propos">A Propos</Link>
             </nav>
-           
+        </div>    
       </header>
     );
   }
