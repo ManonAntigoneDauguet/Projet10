0ba59bb73a754904e5a5d36b009745cdaeff3245
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-11-21 15:55:45 +0100

    feat: ajout de l'image de présentation à la page A-Propos. feat: base de collapse

--- src/pages/A-Propos/index.jsx
+++ src/pages/A-Propos/index.jsx
@@ -1,7 +1,43 @@
+import style from "./apropos.module.scss"
+import chevron from "../../assets/chevron.svg"
+
 function APropos() {
     return (
-      <div>
-        Page A Propos !
+      <div className={ style.apropos__content }>
+
+        <div className={ style.imgContainer }>
+          <div className={ style.imgContainer__img}></div>
+          <div className={ style.imgContainer__bg }></div>
+        </div>
+
+        <div className={ style.txtContainer }>
+          
+          <div className={ style.collapse }>
+            <div className={ style.labelContainer }>
+              <label htmlFor="fiabilite">Fiabilité</label>
+              <img src={ chevron } alt="étendre" />
+            </div>
+            <input type="checkbox" id="fiabilite" name="fiabilite"></input>
+            <div className={ style.detailsContainer }>
+              <p>
+                Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
+                </p>
+            </div>
+          </div>
+
+          <div className={ style.collapse }>
+            <div className={ style.labelContainer }>
+              <label htmlFor="fiabilite">Fiabilité</label>
+              <img src={ chevron } alt="étendre" />
+            </div>
+            <input type="checkbox" id="fiabilite" name="fiabilite"></input>
+            <div className={ style.detailsContainer }>
+              <p>
+                Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
+                </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
