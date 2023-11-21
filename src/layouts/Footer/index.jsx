03c89cb1a7501ccc0d154ea0a67dfdf16e76ccb3
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-11-21 10:36:26 +0100

    feat: ajout de la version responsive de header. refactor: changement d'organisation du css.
    Les fichiers css reliés à un composant sont maintenant des modules du même dossier.

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -1,10 +1,11 @@
 import logo from "../../assets/logo--blanc.svg"
+import style from "./footer.module.scss"
 
 function Footer() {
     return (
       <footer>
-        <div className="footer__content">
-            <img src={logo} alt="Logo Kasa" height="40px"></img>
+        <div className={ style.content }>
+            <img src={ logo } alt="Logo Kasa" height="40px"></img>
             <span>© 2020 Kasa. All right reserved</span>
         </div>    
       </footer>
--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.svg";
+import style from "./header.module.scss";
 
 function Header() {
     return (
       <header>
-        <div className="header__content">
+        <div className={ style.header__content }>
             <img src={logo} alt="Logo Kasa" />
             <nav>
-                <Link to="/" className="current">Accueil</Link>
+                <Link to="/" className={ style.current }>Accueil</Link>
                 <Link to="/a-propos">A Propos</Link>
             </nav>
         </div>    
